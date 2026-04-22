e78bd5c79f2ec58ad0eb2b6140fb84e03fe76196
Pietro Costa Cardoso <pietrocosta.cardoso1@gmail.com>
2026-04-22 19:00:47 -0300

    Restore original content in app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -3,7 +3,7 @@
  * @link: https://github.com/PietroCostaCardoso
  * Copyright (c) 2026. Todos os direitos reservados.
  * Este código está licenciado sob a MIT License.
- * O plágio ou remoção deste cabeçalho é estritamente proibido.
+ * O plágio ou remoção deste cabeçalho é estritamente proibido........
  */
 const FormatUtils = {
     currency(value) {
